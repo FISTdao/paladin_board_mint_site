ac83601d4c6f785a90a00dc783ccd112f15db7c0
FISTdao <98457196+FISTdao@users.noreply.github.com>
2022-03-10 12:52:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import twitterLogo from './assets/twitter-logo.png';
+import CandyMachine from './CandyMachine';
+
+// Constants
+const TWITTER_HANDLE = 'paladinboard';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null)
+
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!')
+
+          // Connect to user's wallet
+          const response = await solana.connect({ onlyIfTrusted: true })
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          )
+
+          // Save user's publicKey to walletAddress state
+          setWalletAddress(response.publicKey.toString())
+        }
+      } else {
+        alert('Solana object not found! Get Phantom Wallet 👻')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const connectWallet = async () => {
+    const { solana } = window
+
+    if (solana) {
+      const response = await solana.connect()
+      const publicKey = response?.publicKey.toString()
+      
+      console.log('Connected with Public Key:', publicKey)
+      setWalletAddress(publicKey)
+    }
+  }
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect wallet
+    </button>
+  )
+
+  // When component first mounts, check if Phantom Wallet is connected
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected()
+    }
+    window.addEventListener('load', onLoad)
+    return () => window.removeEventListener('load', onLoad)
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">⚔️Paladin Board⚔️</p>
+          <p className="sub-text">Mint an NFT on devnet to test out our staking.</p>
+          
+          {/* If wallet is not connected, render Connect Wallet button */}
+          {!walletAddress && renderNotConnectedContainer()}
+        </div>
+        
+        {/* If wallet is connected, render CandyMachine */}
+        { walletAddress && 
+          <CandyMachine walletAddress={window.solana} />
+        }
+
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
